2e4b6474af06eb22480df1563cf0abf7043eccf1
Abhishek Tiwari <abhishek.tiwari10@ltimindtree.com>
2025-11-01 07:35:21 +0000

    Login page and Dashboard

--- webapp/controller/Sap1.controller.js
+++ webapp/controller/Sap1.controller.js
@@ -14,46 +14,32 @@ sap.ui.define([
             oRouter.getRoute("Routedashboard").attachPatternMatched(this._onObjectMatched, this);
 
             // Initialize the user model
-            var oUserModel = new JSONModel();
-            this.getView().setModel(oUserModel, "oUserModel");
-       Event.getParameter("arguments").name;
-            var pass_word = oEvent.getParameter("arguments").pass;
-            var oUserModel = this.getView().getModel("oUserModel");
-            oUserModel.setProperty("/username", user_name);
-            oUserModel.setProperty("/password", pass_word);
+            var oUserModel = new sap.ui.model.json.JSONModel();
+            this.getView().setModel(oUserModel);
+        },
+
+        _onObjectMatched: function (oEvent) {
+            var user_name=oEvent.getParameter("arguments").name;
+            var pass_word = oEvent.getParameter("arguments").pass; // corrected from 'password' to 'pass'
+            this.getView().getModel().setProperty("/username", user_name);
+            this.getView().getModel().setProperty("/password", pass_word);
         },
 
         onNavigateToList: function () {
             let username = this.getView().byId("Input1").getValue();
             let password = this.getView().byId("Input2").getValue();
-
-            if (username === '' || password === '') {
-                MessageBox.error("Invalid Username/Password");
-            } else {
-                var oNorthwindModel = this.getView().getModel("northwind");
-
-                oNorthwindModel.read("/Employees", {
-                    filters: [
-                        new Filter("FirstName", FilterOperator.EQ, username),
-                        new Filter("Extension", FilterOperator.EQ, password)
-                    ],
-                    success: function (oData) {
-                        if (oData.results.length > 0) {
-                            MessageBox.success("Correct credentials");
-                            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-                            oRouter.navTo("Routedashboard", {
-                                name: username,
-                                pass: password
-                            });
-                        } else {
-                            MessageBox.error("Incorrect Username/Password");
-                        }
-                    }.bind(this),
-                    error: function (oError) {
-                        MessageBox.error("Failed to fetch data from Northwind service");
-                    }
-                });
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+           
+            if(username==''||password==''){
+                MessageBox.alert("Incorrect Username");
             }
+            else{ 
+                oRouter.navTo("Routedashboard", {
+                name: username,
+                pass: password // consistent with _onObjectMatched
+            });}
+           
         }
     });
 });
+``
